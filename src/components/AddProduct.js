fd6755f9001032407a4f393536245d6e7cb08ce7
Raj <raj23jarwa@gmail.com>
2024-01-01 20:27:01 +0530

    Form validation

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -5,20 +5,27 @@ const AddProduct = () => {
     const [price, setPrice] = useState('');
     const [category, setCategory] = useState('');
     const [company, setCompany] = useState('');
-    const handleaddproduct=async()=>{
-        console.log(name,price,category,company);
-        const userId = JSON.parse(localStorage.getItem('user'))._id;
-        console.log(userId);
-        
-        let result = await fetch("http://localhost:5000/add-product", {
-    method: "POST",
-    body: JSON.stringify({ name, price, category, company, userId }),
-    headers: {
-        "Content-Type": "application/json"
-    }
-});
+    const [error,setError]=useState(false);
+    const handleaddproduct = async () => {
 
-        result=await result.json();
+        if(!name || !price || !category || !company){
+            setError(true);
+            return false;
+        }
+        
+
+
+        const userId = JSON.parse(localStorage.getItem('user'))._id;
+
+        let result = await fetch("http://localhost:5000/add-product", {
+            method: "POST",
+            body: JSON.stringify({ name, price, category, company, userId }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        result = await result.json();
         console.log(result);
 
     }
@@ -32,6 +39,7 @@ const AddProduct = () => {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
+             {error && !name && <span className='invalid-input'>Enter valid name</span>}
             <input
                 type='text'
                 placeholder='Enter price'
@@ -39,6 +47,8 @@ const AddProduct = () => {
                 value={price}
                 onChange={(e) => setPrice(e.target.value)}
             />
+             {error && !price && <span className='invalid-input'>Enter valid Price</span>}
+
             <input
                 type='text'
                 placeholder='Enter Category'
@@ -46,6 +56,8 @@ const AddProduct = () => {
                 value={category}
                 onChange={(e) => setCategory(e.target.value)}
             />
+             {error && !category && <span className='invalid-input'>Enter valid Category</span>}
+
             <input
                 type='text'
                 placeholder='Enter Company'
@@ -53,6 +65,7 @@ const AddProduct = () => {
                 value={company}
                 onChange={(e) => setCompany(e.target.value)}
             />
+             {error && !company && <span className='invalid-input'>Enter valid company</span>}
             <button className='addproductbtn' type='button' onClick={handleaddproduct}>Add Product</button>
         </div>
     );
