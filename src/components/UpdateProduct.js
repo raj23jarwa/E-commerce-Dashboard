45353cc78e7a185a0800fdb13a6872e2ec01caaa
Raj <raj23jarwa@gmail.com>
2024-01-05 16:06:30 +0530

    Prefilled update Form

--- src/components/UpdateProduct.js
+++ src/components/UpdateProduct.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 const UpdateProduct = () => {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
     const [category, setCategory] = useState('');
     const [company, setCompany] = useState('');
-    const [error,setError]=useState(false);
+    const params=useParams();
+
+    useEffect(()=>{
+     console.log(params);
+     getProductDetails();
+    },[])
+
+    const getProductDetails =async()=>{
+        console.log(params)
+        let result =await fetch(`http://localhost:5000/product/${params.id}`)
+        result=await result.json();
+        setName(result.name)
+        setPrice(result.price)
+        setCategory(result.category);
+        setCompany(result.company);
+    }   
 
     const handleupdateproduct = async () => {
         console.log(name,price,category,company)
